51e1fe8d41130bde011f52b69006564e82e6c136
Shahariarshanto <shahariarshanto@gmail.com>
2023-05-18 12:08:17 +0600

    Regestion Component Added

--- src/components/pages/LoginRegistration/Registration.jsx
+++ src/components/pages/LoginRegistration/Registration.jsx
@@ -5,6 +5,7 @@ const Registration = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [photoURL, setPhotoURL] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
   const handleNameChange = (e) => {
@@ -19,10 +20,14 @@ const Registration = () => {
     setPassword(e.target.value);
   };
 
+  const handlePhotoURLChange = (e) => {
+    setPhotoURL(e.target.value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (name === "" || email === "" || password === "") {
+    if (name === "" || email === "" || password === "" || photoURL === "") {
       setErrorMessage("Please fill in all fields");
       return;
     }
@@ -31,11 +36,13 @@ const Registration = () => {
     console.log("Name:", name);
     console.log("Email:", email);
     console.log("Password:", password);
+    console.log("Photo URL:", photoURL);
 
     // Reset form fields and error message
     setName("");
     setEmail("");
     setPassword("");
+    setPhotoURL("");
     setErrorMessage("");
   };
 
@@ -82,11 +89,24 @@ const Registration = () => {
             required
           />
         </div>
-        {errorMessage && <p className="text-red-500 text-sm">{errorMessage}</p>}
+        <div>
+          <label htmlFor="photoURL" className="block text-gray-700 font-medium">
+            Photo URL
+          </label>
+          <input
+            type="text"
+            id="photoURL"
+            value={photoURL}
+            onChange={handlePhotoURLChange}
+            className="w-full border border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:ring focus:ring-blue-200"
+            required
+          />
+        </div>
+        {errorMessage && <p className="text-sm text-red-500">{errorMessage}</p>}
         <div>
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white font-medium py-2 px-4 rounded-md hover:bg-blue-600 transition-colors"
+            className="w-full bg-primary text-white font-medium py-2 px-4 rounded-md hover:bg-blue-600 transition-colors"
           >
             Register
           </button>
@@ -98,6 +118,35 @@ const Registration = () => {
           Please login
         </Link>
       </p>
+      <div className="text-center my-3">OR</div>
+      <div className="flex justify-center ">
+        <button className=" flex items-center gap-4 text-white font-medium  px-4 rounded-md  transition-colors">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="48"
+            height="48"
+            viewBox="0 0 48 48"
+          >
+            <path
+              fill="#FFC107"
+              d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8c-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C12.955 4 4 12.955 4 24s8.955 20 20 20s20-8.955 20-20c0-1.341-.138-2.65-.389-3.917z"
+            />
+            <path
+              fill="#FF3D00"
+              d="m6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C16.318 4 9.656 8.337 6.306 14.691z"
+            />
+            <path
+              fill="#4CAF50"
+              d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238A11.91 11.91 0 0 1 24 36c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44z"
+            />
+            <path
+              fill="#1976D2"
+              d="M43.611 20.083H42V20H24v8h11.303a12.04 12.04 0 0 1-4.087 5.571l.003-.002l6.19 5.238C36.971 39.205 44 34 44 24c0-1.341-.138-2.65-.389-3.917z"
+            />
+          </svg>
+          Register With Google
+        </button>
+      </div>
     </div>
   );
 };
